4268961243560dd720c86012ed6682a5d7db3532
noodleybroodley <educatedgraph@gmail.com>
2023-08-10 19:30:51 -0400

    fixed cors again

--- server/index.js
+++ server/index.js
@@ -3,10 +3,15 @@ const app = express();
 const SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config();
 
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://cambio.onrender.com'
+}));
+
 let spotifyApi = new SpotifyWebApi({
     clientId: `${process.env.REACT_APP_ID}`,
     clientSecret: `${process.env.REACT_APP_SECRET}`,
-    redirectUri: "http://localhost:3000/"
+    redirectUri: "https://cambio.onrender.com/"
 });
 
 app.get("/api/login", (req, res) => {
